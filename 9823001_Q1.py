cc972f0199591f48451a9bb2ccab231827e61103
Pouya-eb <pouyaebrahimi1379@gmail.com>
2023-07-03 18:13:21 +0430

    Part2 : Class QuadraticEquation (inherited from Equation) is defined

--- 9823001_Q1.py
+++ 9823001_Q1.py
@@ -7,3 +7,11 @@ class Equation:
         return self.a * x + self.b
 
 
+class QuadraticEquation(Equation):
+    def __init__(self, a, b, c):
+        super().__init__(a, b)
+        self.c = c
+
+    def compute(self, x):
+        return self.a * x ** 2 + self.b * x + self.c
+
